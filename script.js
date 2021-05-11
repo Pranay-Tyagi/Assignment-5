e652588c100fff53e0a2c48d0fa708eac2aca6a5
PranayTyagi1 <>
2021-05-11 17:14:46 +0000

    spacing

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.querySelector('button').addEventListener('click', function () {
   let d = document.querySelector('input').value
   d = parseFloat(d)
-  const r = d/2
-  let area = (3.14*(r*r)) * 10
+  const r = d / 2
+  let area = (3.14 * (r * r)) * 10
   let circumference = (2 * 3.14 * r) * 10
   area  = Math.round(area)
   circumference = Math.round(circumference)
-  area = area/10
-  circumference = circumference/10 
+  area = area / 10
+  circumference = circumference / 10 
   alert('Area:  ' + area)
   alert('Circumference:  ' + circumference)
 })
